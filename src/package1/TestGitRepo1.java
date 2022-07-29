aea4837e77048c9a38a5d53d4a4f297ed59824a7
MinuKumari14 <minuchy1406@gmail.com>
2022-07-29 08:54:22 +0530

    change in main method on B

--- src/package1/TestGitRepo1.java
+++ src/package1/TestGitRepo1.java
@@ -3,9 +3,11 @@ package package1;
 public class TestGitRepo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Third project uploaded in Git repository");
-		System.out.println("Third project created in git repository");
+		System.out.println("Third project uploaded in Git repository in Branch B");
+		System.out.println("Third project created in git repository updated in Branch B");
 		System.out.println("Default value");
+		
 	}
+	
 
 }
